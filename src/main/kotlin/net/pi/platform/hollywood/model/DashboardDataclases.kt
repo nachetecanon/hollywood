68b498c6726d377108c32f9351cfce2bf03f6795
xus <jesus.liebana@pibenchmark.com>
2017-09-14 15:38:12 +0200

    280 put limit of charac
    limit of characters on name of widget and dashboard

--- src/main/kotlin/net/pi/platform/hollywood/model/DashboardDataclases.kt
+++ src/main/kotlin/net/pi/platform/hollywood/model/DashboardDataclases.kt
@@ -2,13 +2,15 @@ package net.pi.platform.hollywood.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import javax.validation.Valid
+import javax.validation.constraints.Size
 
 typealias JsonObject = Map<String, Any?>
 
 @Document(collection = "dashboards")
-data class Dashboard(@Id val id: String?, val name: String, val widgets: List<Widget>);
+data class Dashboard(@Id val id: String?, @field:Size(min = 5, max = 60) val name: String, @field:Valid val widgets: List<Widget>);
 
-data class Widget(val id: String?, val name: String, val bookmarked: Boolean, val visualization: JsonObject?, val position: WidgetPosition)
+data class Widget(val id: String?, @field:Size(min = 5, max = 60) val name: String, val bookmarked: Boolean, val visualization: JsonObject?, val position: WidgetPosition)
 
 /**
  * x :Position in the horizontal axis
