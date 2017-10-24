63a57f753d02e494bf8be82b16b343a03fa70c06
xus <jesus.liebana@pibenchmark.com>
2017-10-24 16:39:19 +0200

    PIP-435 add parameter app

--- src/main/kotlin/net/pi/platform/hollywood/model/Widget.kt
+++ src/main/kotlin/net/pi/platform/hollywood/model/Widget.kt
@@ -5,4 +5,4 @@ import javax.validation.constraints.Size
 typealias JsonObject = Map<String, Any?>
 
 data class Widget(val id: String?, @field:Size(min = 5, max = 60) val name: String, val bookmarked: Boolean,
-                  val visualization: JsonObject?, val position: Position)
+                  val visualization: JsonObject?, val app: JsonObject?, val position: Position)
