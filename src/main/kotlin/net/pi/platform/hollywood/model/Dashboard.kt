13c64853f7b403d1b352fb69db29efc4fda92b4f
Andre Dias <andre@p-i.net>
2017-11-01 16:18:05 +0000

    ZRKCRI-274 - Added images to a dashboard

--- src/main/kotlin/net/pi/platform/hollywood/model/Dashboard.kt
+++ src/main/kotlin/net/pi/platform/hollywood/model/Dashboard.kt
@@ -7,4 +7,4 @@ import javax.validation.constraints.Size
 
 @Document(collection = "dashboards")
 data class Dashboard(@Id val id: String?, @field:Size(min = 5, max = 60) val name: String,
-                     @field:Valid val widgets: List<Widget>, val position: Position, val bookmarked: Boolean)
+                     @field:Valid val widgets: List<Widget>, val position: Position, val bookmarked: Boolean, val image: String?)
